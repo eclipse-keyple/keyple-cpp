de05327dff253d611aa168b7c9fe56737c20cb4a
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2018-11-27 00:07:13 +0100

    Removed unwanted directory.
    Removed unwanted cmake-build-debug directory created by CLion IDE.
    
    Signed-off-by: Alexandre MUNSCH <alexandre.munsch@gmail.com>

--- cmake-build-debug/keyple-plugin/pcsc/test/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/amunsch/Documents/GitHub/keyple-cpp/cmake-build-debug/keyple-plugin/pcsc/test/googletest-src
-# Build directory: /Users/amunsch/Documents/GitHub/keyple-cpp/cmake-build-debug/keyple-plugin/pcsc/test/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/keyple-plugin/pcsc/test/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/amunsch/Documents/GitHub/keyple-cpp/cmake-build-debug/keyple-plugin/pcsc/test/googletest-src/googlemock
-# Build directory: /Users/amunsch/Documents/GitHub/keyple-cpp/cmake-build-debug/keyple-plugin/pcsc/test/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
