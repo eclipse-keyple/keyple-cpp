bc73f6a8b0072dcb17130deb825f6be34cdbe997
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2019-01-13 22:03:53 +0100

    Added util files.
    Added util files and fixed compilation issues.
    
    Signed-off-by: Alexandre MUNSCH <alexandre.munsch@gmail.com>

--- java/component/keyple-core/src/main/java/org/eclipse/keyple/util/Configurable.cpp
+++ java/component/keyple-core/src/main/java/org/eclipse/keyple/util/Configurable.cpp
@@ -4,9 +4,6 @@ namespace org {
     namespace eclipse {
         namespace keyple {
             namespace util {
-                using ReaderPlugin = org::eclipse::keyple::seproxy::ReaderPlugin;
-                using KeypleBaseException = org::eclipse::keyple::seproxy::exception::KeypleBaseException;
-                using ProxyReader = org::eclipse::keyple::seproxy::message::ProxyReader;
             }
         }
     }
--- java/component/keyple-core/src/main/java/org/eclipse/keyple/util/Nameable.cpp
+++ java/component/keyple-core/src/main/java/org/eclipse/keyple/util/Nameable.cpp
@@ -4,8 +4,6 @@ namespace org {
     namespace eclipse {
         namespace keyple {
             namespace util {
-                using ReaderPlugin = org::eclipse::keyple::seproxy::ReaderPlugin;
-                using ProxyReader = org::eclipse::keyple::seproxy::message::ProxyReader;
             }
         }
     }
