848e011488123b9552b3b3d5347e45e6742f3070
Alexandre <alexandre.munsch@gmail.com>
2019-05-09 21:57:00 +0200

    Fixed compilation for UseCase_Generic1_ExplicitSelectionAid_Pcsc.
    Signed-off-by: Alexandre MUNSCH <alexandre.munsch@gmail.com>

--- java/example/generic/pc/src/main/java/org/eclipse/keyple/example/generic/pc/UseCase_Generic1_ExplicitSelectionAid_Pcsc.cpp
+++ java/example/generic/pc/src/main/java/org/eclipse/keyple/example/generic/pc/UseCase_Generic1_ExplicitSelectionAid_Pcsc.cpp
@@ -1,115 +1,117 @@
-#include "UseCase_Generic1_ExplicitSelectionAid_Pcsc.h"
-#include "../../../../../../../../../../../../component/keyple-core/src/main/java/org/eclipse/keyple/seproxy/exception/KeypleBaseException.h"
-#include "../../../../../../../../../../../../component/keyple-core/src/main/java/org/eclipse/keyple/seproxy/exception/NoStackTraceThrowable.h"
-#include "../../../../../../../../../../../../component/keyple-core/src/main/java/org/eclipse/keyple/seproxy/SeProxyService.h"
-#include "../../../../../../../../../../../../component/keyple-plugin/pcsc/src/main/java/org/eclipse/keyple/plugin/pcsc/PcscPlugin.h"
-#include "../../../../../../../../../../../../component/keyple-core/src/main/java/org/eclipse/keyple/seproxy/SeReader.h"
+#include "ByteArrayUtils.h"
+#include "ChannelState.h"
+#include "ContactlessProtocols.h"
+#include "KeypleBaseException.h"
+#include "MatchingSe.h"
+#include "NoStackTraceThrowable.h"
+#include "PcscPlugin.h"
 #include "ReaderUtilities.h"
-#include "../../../../../../../../../../../../component/keyple-core/src/main/java/org/eclipse/keyple/seproxy/ChannelState.h"
-#include "../../../../../../../../../../../../component/keyple-core/src/main/java/org/eclipse/keyple/seproxy/SeSelector.h"
-#include "../../../../../../../../../../../../component/keyple-core/src/main/java/org/eclipse/keyple/seproxy/protocol/ContactlessProtocols.h"
-#include "../../../../../../../../../../../../component/keyple-core/src/main/java/org/eclipse/keyple/util/ByteArrayUtils.h"
+#include "SelectionStatus.h"
+#include "SeProtocol.h"
+#include "SeProxyService.h"
+#include "SeReader.h"
+#include "SeSelection.h"
+#include "SeSelectionRequest.h"
+#include "SeSelector.h"
 
-namespace org {
-    namespace eclipse {
-        namespace keyple {
-            namespace example {
-                namespace generic_Renamed {
-                    namespace pc {
-                        using PcscPlugin = org::eclipse::keyple::plugin::pcsc::PcscPlugin;
-                        using ChannelState = org::eclipse::keyple::seproxy::ChannelState;
-                        using SeProxyService = org::eclipse::keyple::seproxy::SeProxyService;
-                        using SeReader = org::eclipse::keyple::seproxy::SeReader;
-                        using SeSelector = org::eclipse::keyple::seproxy::SeSelector;
-                        using KeypleBaseException = org::eclipse::keyple::seproxy::exception::KeypleBaseException;
-                        using NoStackTraceThrowable = org::eclipse::keyple::seproxy::exception::NoStackTraceThrowable;
-                        using ContactlessProtocols = org::eclipse::keyple::seproxy::protocol::ContactlessProtocols;
-                        using namespace org::eclipse::keyple::transaction;
-                        using ByteArrayUtils = org::eclipse::keyple::util::ByteArrayUtils;
-                        using org::slf4j::Logger;
-                        using org::slf4j::LoggerFactory;
-const std::shared_ptr<org::slf4j::Logger> UseCase_Generic1_ExplicitSelectionAid_Pcsc::logger = org::slf4j::LoggerFactory::getLogger(UseCase_Generic1_ExplicitSelectionAid_Pcsc::typeid);
-std::string UseCase_Generic1_ExplicitSelectionAid_Pcsc::seAid = "A0000004040125090101";
+using ByteArrayUtils        = org::eclipse::keyple::util::ByteArrayUtils;
+using ChannelState          = org::eclipse::keyple::seproxy::ChannelState;
+using ContactlessProtocols  = org::eclipse::keyple::seproxy::protocol::ContactlessProtocols;
+using KeypleBaseException   = org::eclipse::keyple::seproxy::exception::KeypleBaseException;
+using Logger                = org::eclipse::keyple::common::Logger;
+using MatchingSe            = org::eclipse::keyple::transaction::MatchingSe;
+using NoStackTraceThrowable = org::eclipse::keyple::seproxy::exception::NoStackTraceThrowable;
+using PcscPlugin            = org::eclipse::keyple::plugin::pcsc::PcscPlugin;
+using ReaderUtilities       = org::eclipse::keyple::example::generic::pc::ReaderUtilities;
+using SelectionStatus       = org::eclipse::keyple::seproxy::message::SelectionStatus;
+using SeProtocol            = org::eclipse::keyple::seproxy::protocol::SeProtocol;
+using SeProxyService        = org::eclipse::keyple::seproxy::SeProxyService;
+using SeReader              = org::eclipse::keyple::seproxy::SeReader;
+using SeSelector            = org::eclipse::keyple::seproxy::SeSelector;
+using SeSelection           = org::eclipse::keyple::transaction::SeSelection;
+using SeSelectionRequest    = org::eclipse::keyple::transaction::SeSelectionRequest;
 
-                        void UseCase_Generic1_ExplicitSelectionAid_Pcsc::main(std::vector<std::string> &args) throw(KeypleBaseException, InterruptedException, IOException, NoStackTraceThrowable) {
+static std::string seAid = "A0000004040125090101";
 
-                            /* Get the instance of the SeProxyService (Singleton pattern) */
-                            std::shared_ptr<SeProxyService> seProxyService = SeProxyService::getInstance();
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
 
-                            /* Get the instance of the PC/SC plugin */
-                            std::shared_ptr<PcscPlugin> pcscPlugin = PcscPlugin::getInstance();
+    /* Get the instance of the PC/SC plugin */
+    PcscPlugin pcscplugin = PcscPlugin::getInstance();
+    pcscplugin.initReaders();
 
-                            /* Assign PcscPlugin to the SeProxyService */
-                            seProxyService->addPlugin(pcscPlugin);
+    /* Get the instance of the SeProxyService (Singleton pattern) and assign PcscPlugin to the SeProxyService */
+    SeProxyService& seProxyService = SeProxyService::getInstance();
+    seProxyService.addPlugin(std::make_shared<PcscPlugin>(PcscPlugin::getInstance()));
 
-                            /*
-                             * Get a SE reader ready to work with generic SE. Use the getReader helper method from the
-                             * ReaderUtilities class.
-                             */
-                            std::shared_ptr<SeReader> seReader = ReaderUtilities::getDefaultContactLessSeReader(seProxyService);
+    /*
+     * Get a SE reader ready to work with generic SE. Use the getReader helper method from the
+     * ReaderUtilities class.
+     */
+    std::shared_ptr<SeReader> seReader = ReaderUtilities::getDefaultContactLessSeReader(std::shared_ptr<SeProxyService>(&seProxyService));
 
-                            /* Check if the reader exists */
-                            if (seReader == nullptr) {
-                                throw std::make_shared<IllegalStateException>("Bad SE reader setup");
-                            }
+    /* Check if the reader exists */
+    if (seReader == nullptr) {
+        throw IllegalStateException("Bad SE reader setup");
+    }
 
-                            logger->info("=============== UseCase Generic #1: AID based explicit selection ==================");
-                            logger->info("= SE Reader  NAME = {}", seReader->getName());
+    std::cout << "=============== UseCase Generic #1: AID based explicit selection ==================" << std::endl;
+    std::cout << "= SE Reader  NAME = " << seReader->getName() << std::endl;
 
-                            /* Check if a SE is present in the reader */
-                            if (seReader->isSePresent()) {
+    /* Check if a SE is present in the reader */
+    if (seReader->isSePresent()) {
 
-                                logger->info("==================================================================================");
-                                logger->info("= AID based selection.                                                           =");
-                                logger->info("==================================================================================");
+        std::cout << "==================================================================================" << std::endl;
+        std::cout << "= AID based selection.                                                           =" << std::endl;
+        std::cout << "==================================================================================" << std::endl;
 
-                                /*
-                                 * Prepare the SE selection
-                                 */
-                                std::shared_ptr<SeSelection> seSelection = std::make_shared<SeSelection>(seReader);
+        /*
+            * Prepare the SE selection
+            */
+        std::shared_ptr<SeSelection> seSelection = std::make_shared<SeSelection>(seReader);
 
-                                /*
-                                 * Setting of an AID based selection (in this example a Calypso REV3 PO)
-                                 *
-                                 * Select the first application matching the selection AID whatever the SE communication
-                                 * protocol keep the logical channel open after the selection
-                                 */
+        /*
+            * Setting of an AID based selection (in this example a Calypso REV3 PO)
+            *
+            * Select the first application matching the selection AID whatever the SE communication
+            * protocol keep the logical channel open after the selection
+            */
 
-                                /*
-                                 * Generic selection: configures a SeSelector with all the desired attributes to make
-                                 * the selection and read additional information afterwards
-                                 */
-                                std::shared_ptr<SeSelectionRequest> seSelectionRequest = std::make_shared<SeSelectionRequest>(std::make_shared<SeSelector>(std::make_shared<SeSelector::AidSelector>(ByteArrayUtils::fromHex(seAid), nullptr), nullptr, "AID: " + seAid), ChannelState::KEEP_OPEN, ContactlessProtocols::PROTOCOL_ISO14443_4);
+        /*
+            * Generic selection: configures a SeSelector with all the desired attributes to make
+            * the selection and read additional information afterwards
+            */
+        std::vector<char> aid = ByteArrayUtils::fromHex(seAid);
+        std::shared_ptr<SeSelector::AidSelector> aidSelector = std::make_shared<SeSelector::AidSelector>(aid, nullptr);
+        std::shared_ptr<SeSelector> selector = std::make_shared<SeSelector>(aidSelector, nullptr, StringHelper::formatSimple("AID: %s", seAid));
+        std::shared_ptr<SeSelectionRequest> seSelectionRequest = std::make_shared<SeSelectionRequest>(selector, ChannelState::KEEP_OPEN, std::shared_ptr<SeProtocol>(&ContactlessProtocols::PROTOCOL_ISO14443_4));
 
-                                /*
-                                 * Add the selection case to the current selection (we could have added other cases
-                                 * here)
-                                 */
-                                std::shared_ptr<MatchingSe> matchingSe = seSelection->prepareSelection(seSelectionRequest);
+        /*
+            * Add the selection case to the current selection (we could have added other cases
+            * here)
+            */
+        std::shared_ptr<MatchingSe> matchingSe = seSelection->prepareSelection(seSelectionRequest);
 
-                                /*
-                                 * Actual SE communication: operate through a single request the SE selection
-                                 */
-                                if (seSelection->processExplicitSelection()) {
-                                    logger->info("The selection of the SE has succeeded.");
-                                    logger->info("Application FCI = {}", matchingSe->getSelectionSeResponse()->getSelectionStatus()->getFci());
+        /*
+            * Actual SE communication: operate through a single request the SE selection
+            */
+        if (seSelection->processExplicitSelection()) {
+            std::cout << "The selection of the SE has succeeded." << std::endl;
+            std::cout << "Application FCI = " << matchingSe->getSelectionSeResponse()->getSelectionStatus()->getFci() << std::endl;
 
-                                    logger->info("==================================================================================");
-                                    logger->info("= End of the generic SE processing.                                              =");
-                                    logger->info("==================================================================================");
-                                }
-                                else {
-                                    logger->error("The selection of the SE has failed.");
-                                }
-                            }
-                            else {
-                                logger->error("No SE were detected.");
-                            }
-                            exit(0);
-                        }
-                    }
-                }
-            }
+            std::cout << "==================================================================================" << std::endl;
+            std::cout << "= End of the generic SE processing.                                              =" << std::endl;
+            std::cout << "==================================================================================" << std::endl;
+        }
+        else {
+            std::cout << "The selection of the SE has failed." << std::endl;
         }
     }
+    else {
+        std::cout << "No SE were detected." << std::endl;
+    }
+   
+    return 0;
 }
