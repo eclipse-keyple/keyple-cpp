b49a107a9d7fa10217cab4d3940f283912ebfae9
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2020-02-19 16:40:48 +0100

    Fixed previous commit.
    Signed-off-by: Alexandre MUNSCH <alexandre.munsch@gmail.com>

--- component/keyple-calypso/src/main/command/po/parser/security/OpenSession10RespPars.cpp
+++ component/keyple-calypso/src/main/command/po/parser/security/OpenSession10RespPars.cpp
@@ -1,14 +1,16 @@
-/********************************************************************************
-* Copyright (c) 2018 Calypso Networks Association https://www.calypsonet-asso.org/
-*
-* See the NOTICE file(s) distributed with this work for additional information regarding copyright
-* ownership.
-*
-* This program and the accompanying materials are made available under the terms of the Eclipse
-* Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
-*
-* SPDX-License-Identifier: EPL-2.0
-********************************************************************************/
+/******************************************************************************
+ * Copyright (c) 2018 Calypso Networks Association                            *
+ * https://www.calypsonet-asso.org/                                           *
+ *                                                                            *
+ * See the NOTICE file(s) distributed with this work for additional           *
+ * information regarding copyright ownership.                                 *
+ *                                                                            *
+ * This program and the accompanying materials are made available under the   *
+ * terms of the Eclipse Public License 2.0 which is available at              *
+ * http://www.eclipse.org/legal/epl-2.0                                       *
+ *                                                                            *
+ * SPDX-License-Identifier: EPL-2.0                                           *
+ ******************************************************************************/
 
 #include "OpenSession10RespPars.h"
 #include "ApduResponse.h"
@@ -26,24 +28,30 @@ namespace security {
 using namespace keyple::calypso::command::po;
 using namespace keyple::core::seproxy::message;
 
-OpenSession10RespPars::OpenSession10RespPars(std::shared_ptr<ApduResponse> response)
+OpenSession10RespPars::OpenSession10RespPars(
+  std::shared_ptr<ApduResponse> response)
 : AbstractOpenSessionRespPars(response, PoRevision::REV1_0)
 {
-    std::vector<char> data = response->getDataOut();
+    std::vector<uint8_t> data = response->getDataOut();
     this->secureSession = toSecureSession(data);
 }
 
-std::shared_ptr<AbstractOpenSessionRespPars::SecureSession> OpenSession10RespPars::toSecureSession(std::vector<char> &apduResponseData)
+std::shared_ptr<AbstractOpenSessionRespPars::SecureSession>
+    OpenSession10RespPars::toSecureSession(
+        const std::vector<uint8_t>& apduResponseData)
 {
     return createSecureSession(apduResponseData);
 }
 
-std::shared_ptr<AbstractOpenSessionRespPars::SecureSession> OpenSession10RespPars::createSecureSession(std::vector<char> &apduResponseData)
+std::shared_ptr<AbstractOpenSessionRespPars::SecureSession>
+    OpenSession10RespPars::createSecureSession(
+        const std::vector<uint8_t>& apduResponseData)
 {
     bool previousSessionRatified = true;
 
     /**
-     * In rev 1.0 mode, the response to the Open Secure Session command is as follows:
+     * In rev 1.0 mode, the response to the Open Secure Session command is as
+     * follows:
      * <p>
      * <code>CC CC CC CC [RR RR] [NN..NN]</code>
      * <p>
@@ -57,8 +65,10 @@ std::shared_ptr<AbstractOpenSessionRespPars::SecureSession> OpenSession10RespPar
      * <ul>
      * <li>4: ratified, 4-byte challenge, no data</li>
      * <li>33: ratified, 4-byte challenge, 29 bytes of data</li>
-     * <li>6: not ratified (2 ratification bytes), 4-byte challenge, no data</li>
-     * <li>35 not ratified (2 ratification bytes), 4-byte challenge, 29 bytes of data</li>
+     * <li>6: not ratified (2 ratification bytes), 4-byte challenge, no data
+     * </li>
+     * <li>35 not ratified (2 ratification bytes), 4-byte challenge, 29 bytes of
+     * data</li>
      * </ul>
      */
     switch (apduResponseData.size()) {
@@ -71,19 +81,22 @@ std::shared_ptr<AbstractOpenSessionRespPars::SecureSession> OpenSession10RespPar
             previousSessionRatified = false;
             break;
         default:
-            throw IllegalStateException("Bad response length to Open Secure Session: " +
-                                        std::to_string(apduResponseData.size()));
+            throw IllegalStateException(
+                      "Bad response length to Open Secure Session: " +
+                      std::to_string(apduResponseData.size()));
     }
 
     /* KVC doesn't exist and is set to null for this type of PO */
-    std::vector<char> emptyVector;
-    std::vector<char> challengeTransactionCounter = Arrays::copyOfRange(apduResponseData, 1, 4);
-    std::vector<char> challengeRandomNumber = Arrays::copyOfRange(apduResponseData, 4, 5);
+    std::vector<uint8_t> emptyVector;
+    std::vector<uint8_t> challengeTransactionCounter =
+        Arrays::copyOfRange(apduResponseData, 1, 4);
+    std::vector<uint8_t> challengeRandomNumber =
+        Arrays::copyOfRange(apduResponseData, 4, 5);
 
-    return std::make_shared<SecureSession>(challengeTransactionCounter,
-                                           challengeRandomNumber,
-                                           previousSessionRatified, false, nullptr, emptyVector,
-                                           apduResponseData);
+    return std::make_shared<SecureSession>(
+               challengeTransactionCounter, challengeRandomNumber,
+               previousSessionRatified, false, nullptr, emptyVector,
+               apduResponseData);
 }
 
 }
--- component/keyple-calypso/src/main/command/po/parser/security/OpenSession32RespPars.cpp
+++ component/keyple-calypso/src/main/command/po/parser/security/OpenSession32RespPars.cpp
@@ -1,3 +1,17 @@
+/******************************************************************************
+ * Copyright (c) 2018 Calypso Networks Association                            *
+ * https://www.calypsonet-asso.org/                                           *
+ *                                                                            *
+ * See the NOTICE file(s) distributed with this work for additional           *
+ * information regarding copyright ownership.                                 *
+ *                                                                            *
+ * This program and the accompanying materials are made available under the   *
+ * terms of the Eclipse Public License 2.0 which is available at              *
+ * http://www.eclipse.org/legal/epl-2.0                                       *
+ *                                                                            *
+ * SPDX-License-Identifier: EPL-2.0                                           *
+ ******************************************************************************/
+
 #include "OpenSession32RespPars.h"
 #include "ApduResponse.h"
 #include "AbstractOpenSessionRespPars.h"
@@ -15,38 +29,53 @@ using namespace keyple::calypso::command::po;
 using namespace keyple::core::seproxy::message;
 using namespace keyple::calypso::command::po::parser::security;
 
-OpenSession32RespPars::OpenSession32RespPars(std::shared_ptr<ApduResponse> response)
+OpenSession32RespPars::OpenSession32RespPars(
+  std::shared_ptr<ApduResponse> response)
 : AbstractOpenSessionRespPars(response, PoRevision::REV3_2)
 {
-    std::vector<char> data = response->getDataOut();
+    std::vector<uint8_t> data = response->getDataOut();
     this->secureSession = toSecureSession(data);
 }
 
-std::shared_ptr<AbstractOpenSessionRespPars::SecureSession> OpenSession32RespPars::toSecureSession(std::vector<char> &apduResponseData)
+std::shared_ptr<AbstractOpenSessionRespPars::SecureSession>
+    OpenSession32RespPars::toSecureSession(
+        const std::vector<uint8_t>& apduResponseData)
 {
     return createSecureSession(apduResponseData);
 }
 
-std::shared_ptr<AbstractOpenSessionRespPars::SecureSession> OpenSession32RespPars::createSecureSession(std::vector<char> &apduResponse)
+std::shared_ptr<AbstractOpenSessionRespPars::SecureSession>
+    OpenSession32RespPars::createSecureSession(
+        const std::vector<uint8_t>& apduResponse)
 {
 
     char flag = apduResponse[8];
-    // ratification: if the bit 0 of flag is set then the previous security has been ratified
-    bool previousSessionRatified = (flag & (1 << 0)) == static_cast<char>(0x00);
-    // secure security: if the bit 1 of flag is set then the secure security is authorized
-    bool manageSecureSessionAuthorized = (flag & (1 << 1)) == static_cast<char>(0x02);
+    /*
+     * ratification: if the bit 0 of flag is set then the previous security has
+     * been ratified
+     */
+    bool previousSessionRatified = (flag & (1 << 0)) == 0x00;
+    /*
+     * secure security: if the bit 1 of flag is set then the secure security is
+     * authorized
+     */
+    bool manageSecureSessionAuthorized = (flag & (1 << 1)) == 0x02;
 
     char kif = apduResponse[9];
     int dataLength = apduResponse[11];
-    std::vector<char> data = Arrays::copyOfRange(apduResponse, 12, 12 + dataLength);
-    std::vector<char> challengeTransactionCounter = Arrays::copyOfRange(apduResponse, 0, 3);
-    std::vector<char> challengeRandomNumber = Arrays::copyOfRange(apduResponse, 3, 8);
 
-    return std::make_shared<SecureSession>(challengeTransactionCounter,
-                                           challengeRandomNumber,
-                                           previousSessionRatified, manageSecureSessionAuthorized,
-                                           kif, std::make_shared<Byte>(apduResponse[10]),
-                                           data, apduResponse);
+    std::vector<uint8_t> data =
+        Arrays::copyOfRange(apduResponse, 12, 12 + dataLength);
+    std::vector<uint8_t> challengeTransactionCounter =
+        Arrays::copyOfRange(apduResponse, 0, 3);
+    std::vector<uint8_t> challengeRandomNumber =
+        Arrays::copyOfRange(apduResponse, 3, 8);
+
+    return std::make_shared<SecureSession>(
+               challengeTransactionCounter, challengeRandomNumber,
+               previousSessionRatified, manageSecureSessionAuthorized,
+               kif, std::make_shared<Byte>(apduResponse[10]),
+               data, apduResponse);
 }
 
 }
