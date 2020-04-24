4820f4b5308410fd7cf389d4f52ff236324ee6f8
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2020-04-24 12:21:44 +0200

    Keyple 0.8.0 - Fixed Windows CLang CMake toolchain.
    Signed-off-by: Alexandre MUNSCH <alexandre.munsch@gmail.com>

--- toolchain/clang-windows.cmake
+++ toolchain/clang-windows.cmake
@@ -25,10 +25,29 @@ SET(CMAKE_CXX_COMPILER cl.exe)
 #set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
 
 # Compiler flags
-SET(CMAKE_CXX_FLAGS "")
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")     # warning level
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zi")     # debug symbols
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")   # exception handling
+#SET(CMAKE_CXX_FLAGS "")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3")          # warning level
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zi")          # debug symbols
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Od")          # disables optimization
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Ob0")         # controls inline expans.
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Oy-")         # rem. omit frame pointer
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /D WIN32")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /D _WINDOWS")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /D _WINDLL")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /D _MBCS")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Gm-")         # rem. minimal rebuild
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")        # exception handling
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /RTC1")        # runtime error check
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MDd")         # debug multi thread dll
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /GS")          # buffers security check
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /fp:precise")  # floating point
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zc:wchar_t")  # standard behaviour
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zc:forScope") # =
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zc:inline")   # =
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /GR")          # enables RTTI
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Gd")          # uses __cdecl convention
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /TP")          # all sources are C++
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /analyze-")    # rem. code analysis
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4275")
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4251")
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4290")
@@ -41,4 +60,7 @@ SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4309")
 
 # Libraries type
 SET(LIBRARY_TYPE SHARED)
+
+# Special tricks
+INCLUDE(GenerateExportHeader)
 
