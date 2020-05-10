edfa5d184359b4fe80ec8dcd866a63440272e6a0
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2020-05-10 20:42:09 +0200

    feat: added unit testing for AbstractIso7816CommandBuilder class
    Signed-off-by: Alexandre MUNSCH <alexandre.munsch@gmail.com>

---
+++ component/keyple-core/src/test/command/AbstractIso7816CommandBuilderTest.cpp
@@ -0,0 +1,267 @@
+/******************************************************************************
+ * Copyright (c) 2018 Calypso Networks Association                            *
+ * https://www.calypsonet-asso.org/                                           *
+ *                                                                            *
+ * See the NOTICE file(s) distributed with this work for additional           *
+ * information regarding copyright ownership.                                 *
+ *                                                                            *
+ * This program and the accompanying materials are made available under the   *
+ * terms of the Eclipse Public License 2.0 which is available at              *
+ * http://www.eclipse.org/legal/epl-2.0                                       *
+ *                                                                            *
+ * SPDX-License-Identifier: EPL-2.0                                           *
+ ******************************************************************************/
+
+#include <unordered_map>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "AbstractIso7816CommandBuilder.h"
+
+using namespace keyple::core::command;
+
+using namespace testing;
+
+class CommandsTableMock : public CommandsTable {
+public:
+    MOCK_METHOD(const std::string&, getName, (), (const, override));
+    MOCK_METHOD(uint8_t, getInstructionByte, (), (const, override));
+    MOCK_METHOD(const std::type_info&, getCommandBuilderClass, (),
+                (const, override));
+    MOCK_METHOD(const std::type_info&, getResponseParserClass, (),
+                (const, override));
+};
+
+class AbstractIso7816CommandBuilderMock
+: public AbstractIso7816CommandBuilder {
+public:
+    AbstractIso7816CommandBuilderMock(CommandsTable& commandReference,
+                                      std::shared_ptr<ApduRequest> request)
+    : AbstractIso7816CommandBuilder(commandReference, request)
+    {
+
+    }
+
+    AbstractIso7816CommandBuilderMock(const std::string& name,
+                                      std::shared_ptr<ApduRequest> request)
+    : AbstractIso7816CommandBuilder(name, request)
+    {
+
+    }
+
+    std::shared_ptr<ApduRequest> setApduRequest(
+        uint8_t cla, const CommandsTable& command, uint8_t p1,
+        uint8_t p2, const std::vector<uint8_t>& dataIn)
+    {
+        return AbstractIso7816CommandBuilder::setApduRequest(cla, command, p1,
+                                                             p2, dataIn);
+    }
+
+    std::shared_ptr<ApduRequest> setApduRequest(
+        const uint8_t cla, const CommandsTable& command, const uint8_t p1,
+        const uint8_t p2, const std::vector<uint8_t>& dataIn, const uint8_t le)
+    {
+        return AbstractIso7816CommandBuilder::setApduRequest(cla, command, p1,
+	                                                     p2, dataIn, le);
+    }
+
+    std::shared_ptr<ApduRequest> setApduRequest(
+        uint8_t cla, const CommandsTable& command, uint8_t p1, uint8_t p2,
+        uint8_t le)
+    {
+        return AbstractIso7816CommandBuilder::setApduRequest(cla, command, p1,
+                                                             p2, le);
+    }
+};
+
+TEST(AbstractIso7816CommandBuilderTest, AbstractIso7816CommandBuilder1)
+{
+    /* Instantiate AbstractApduCommandBuilder using first constructor */
+
+    CommandsTableMock table;
+    const std::vector<uint8_t> apdu = {0x11, 0x22, 0x33, 0X44, 0x90, 0x00};
+    std::shared_ptr<ApduRequest> request =
+        std::make_shared<ApduRequest>(apdu, false);
+
+    const std::string name = "CommandsTableMock";
+    EXPECT_CALL(table, getName()).WillRepeatedly(ReturnRef(name));
+
+    AbstractIso7816CommandBuilder builder(table, request);
+}
+
+TEST(AbstractIso7816CommandBuilderTest,
+     AbstractIso7816CommandBuilder1_setApduRequest_case2)
+{
+    /* Instantiate AbstractApduCommandBuilder using first constructor */
+
+    CommandsTableMock table;
+    const std::vector<uint8_t> apdu = {0x11};
+    const std::vector<uint8_t> ref = {0xcc, 0x11, 0xb1, 0xb2, 0x06};
+
+    std::shared_ptr<ApduRequest> request =
+        std::make_shared<ApduRequest>(apdu, false);
+
+    const std::string name = "CommandsTableMock";
+    EXPECT_CALL(table, getName()).WillRepeatedly(ReturnRef(name));
+
+    AbstractIso7816CommandBuilderMock builder(table, request);
+
+    EXPECT_CALL(table, getInstructionByte()).WillRepeatedly(Return(0x11));
+
+    /* case 2: dataIn = null, le != null */
+    std::shared_ptr<ApduRequest> req =
+        builder.setApduRequest(0xcc, table, 0x0b1, 0x0b2, 0x06);
+
+    ASSERT_EQ(req->getBytes(), ref);
+}
+
+
+TEST(AbstractIso7816CommandBuilderTest,
+     AbstractIso7816CommandBuilder1_setApduRequest_case3)
+{
+    /* Instantiate AbstractApduCommandBuilder using first constructor */
+
+    CommandsTableMock table;
+    const std::vector<uint8_t> apdu = {0x11, 0x22, 0x33, 0x44};
+    const std::vector<uint8_t> ref = {0xcc, 0x11, 0xb1, 0xb2, 0x04, 0x11, 0x22,
+                                      0x33, 0x44};
+
+    std::shared_ptr<ApduRequest> request =
+        std::make_shared<ApduRequest>(apdu, false);
+
+    const std::string name = "CommandsTableMock";
+    EXPECT_CALL(table, getName()).WillRepeatedly(ReturnRef(name));
+
+    AbstractIso7816CommandBuilderMock builder(table, request);
+
+    EXPECT_CALL(table, getInstructionByte()).WillRepeatedly(Return(0x11));
+
+    /* case 3: dataIn != null, le = null*/
+    std::shared_ptr<ApduRequest> req =
+        builder.setApduRequest(0xcc, table, 0xb1, 0xb2, apdu);
+
+    ASSERT_EQ(req->getBytes(), ref);
+}
+
+
+TEST(AbstractIso7816CommandBuilderTest,
+     AbstractIso7816CommandBuilder1_setApduRequest_case4)
+{
+    /* Instantiate AbstractApduCommandBuilder using first constructor */
+
+    CommandsTableMock table;
+    const std::vector<uint8_t> apdu = {0x11, 0x22, 0x33, 0x44};
+    const std::vector<uint8_t> ref = {0xcc, 0x11, 0xb1, 0xb2, 0x04, 0x11, 0x22,
+                                      0x33, 0x44, 0x00};
+
+    std::shared_ptr<ApduRequest> request =
+        std::make_shared<ApduRequest>(apdu, false);
+
+    const std::string name = "CommandsTableMock";
+    EXPECT_CALL(table, getName()).WillRepeatedly(ReturnRef(name));
+
+    AbstractIso7816CommandBuilderMock builder(table, request);
+
+    EXPECT_CALL(table, getInstructionByte()).WillRepeatedly(Return(0x11));
+
+    /* case 4: dataIn = null, le = 0 */
+    std::shared_ptr<ApduRequest> req =
+        builder.setApduRequest(0xcc, table, 0xb1, 0xb2, apdu, 0x00);
+
+    ASSERT_EQ(req->getBytes(), ref);
+}
+
+TEST(AbstractIso7816CommandBuilderTest, AbstractIso7816CommandBuilder2)
+{
+    /* Instantiate AbstractApduCommandBuilder using second constructor */
+
+    const std::vector<uint8_t> apdu = {0x11, 0x22, 0x33, 0X44, 0x90, 0x00};
+    std::shared_ptr<ApduRequest> request =
+        std::make_shared<ApduRequest>(apdu, false);
+    const std::string name = "AbstractIso7816CommandBuilder2";
+
+    AbstractIso7816CommandBuilder builder(name, request);
+}
+
+TEST(AbstractIso7816CommandBuilderTest,
+     AbstractIso7816CommandBuilder2_setApduRequest_case2)
+{
+    /* Instantiate AbstractApduCommandBuilder using second constructor */
+
+    CommandsTableMock table;
+    const std::vector<uint8_t> apdu = {0x11};
+    const std::vector<uint8_t> ref = {0xcc, 0x11, 0xb1, 0xb2, 0x06};
+
+    std::shared_ptr<ApduRequest> request =
+        std::make_shared<ApduRequest>(apdu, false);
+
+    const std::string name = "CommandsTableMock";
+    EXPECT_CALL(table, getName()).WillRepeatedly(ReturnRef(name));
+
+    AbstractIso7816CommandBuilderMock builder(name, request);
+
+    EXPECT_CALL(table, getInstructionByte()).WillRepeatedly(Return(0x11));
+
+    /* case 2: dataIn = null, le != null */
+    std::shared_ptr<ApduRequest> req =
+        builder.setApduRequest(0xcc, table, 0x0b1, 0x0b2, 0x06);
+
+    ASSERT_EQ(req->getBytes(), ref);
+}
+
+
+TEST(AbstractIso7816CommandBuilderTest,
+     AbstractIso7816CommandBuilder2_setApduRequest_case3)
+{
+    /* Instantiate AbstractApduCommandBuilder using second constructor */
+
+    CommandsTableMock table;
+    const std::vector<uint8_t> apdu = {0x11, 0x22, 0x33, 0x44};
+    const std::vector<uint8_t> ref = {0xcc, 0x11, 0xb1, 0xb2, 0x04, 0x11, 0x22,
+                                      0x33, 0x44};
+
+    std::shared_ptr<ApduRequest> request =
+        std::make_shared<ApduRequest>(apdu, false);
+
+    const std::string name = "CommandsTableMock";
+    EXPECT_CALL(table, getName()).WillRepeatedly(ReturnRef(name));
+
+    AbstractIso7816CommandBuilderMock builder(name, request);
+
+    EXPECT_CALL(table, getInstructionByte()).WillRepeatedly(Return(0x11));
+
+    /* case 3: dataIn != null, le = null*/
+    std::shared_ptr<ApduRequest> req =
+        builder.setApduRequest(0xcc, table, 0xb1, 0xb2, apdu);
+
+    ASSERT_EQ(req->getBytes(), ref);
+}
+
+
+TEST(AbstractIso7816CommandBuilderTest,
+     AbstractIso7816CommandBuilder2_setApduRequest_case4)
+{
+    /* Instantiate AbstractApduCommandBuilder using second constructor */
+
+    CommandsTableMock table;
+    const std::vector<uint8_t> apdu = {0x11, 0x22, 0x33, 0x44};
+    const std::vector<uint8_t> ref = {0xcc, 0x11, 0xb1, 0xb2, 0x04, 0x11, 0x22,
+                                      0x33, 0x44, 0x00};
+
+    std::shared_ptr<ApduRequest> request =
+        std::make_shared<ApduRequest>(apdu, false);
+
+    const std::string name = "CommandsTableMock";
+    EXPECT_CALL(table, getName()).WillRepeatedly(ReturnRef(name));
+
+    AbstractIso7816CommandBuilderMock builder(name, request);
+
+    EXPECT_CALL(table, getInstructionByte()).WillRepeatedly(Return(0x11));
+
+    /* case 4: dataIn = null, le = 0 */
+    std::shared_ptr<ApduRequest> req =
+        builder.setApduRequest(0xcc, table, 0xb1, 0xb2, apdu, 0x00);
+
+    ASSERT_EQ(req->getBytes(), ref);
+}
