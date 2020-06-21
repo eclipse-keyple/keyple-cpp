e78c61398652b0a1009def0876d19af620f47fda
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2020-06-21 22:57:11 +0200

    feat: added ReaderPlugin class unitary tests
    Signed-off-by: Alexandre MUNSCH <alexandre.munsch@gmail.com>

---
+++ component/keyple-core/src/test/seproxy/ReaderPluginTest.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+ * Copyright (c) 2018 Calypso Networks Association                            *
+ * https://www.calypsonet-asso.org/                                           *
+ *                                                                            *
+ * See the NOTICE file(s) distributed with this work for additional           *
+ * information regarding copyright ownership.                                 *
+ *                                                                            *
+ * This program and the accompanying materials are made available under the   *
+ * terms of the Eclipse Public License 2.0 which is available at              *
+ * http://www.eclipse.org/legal/epl-2.0                                       *
+ *                                                                            *
+ * SPDX-License-Identifier: EPL-2.0                                           *
+ ******************************************************************************/
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "ReaderPlugin.h"
+
+using namespace testing;
+
+using namespace keyple::core::seproxy;
+
+class RP_ReaderPluginMock : public ReaderPlugin {
+public:
+    MOCK_METHOD((const std::set<std::string>), getReaderNames, (),
+                (const, override));
+
+    MOCK_METHOD((std::set<std::shared_ptr<SeReader>>&), getReaders, (),
+                (override));
+
+    MOCK_METHOD((const std::shared_ptr<SeReader>), getReader,
+                (const std::string& name), (const, override));
+
+    MOCK_METHOD((const std::map<const std::string, const std::string>),
+                getParameters, (), (const, override));
+
+    MOCK_METHOD(void, setParameter,
+                (const std::string& key, const std::string& value), (override));
+
+    MOCK_METHOD(void, setParameters,
+                ((const std::map<const std::string,
+                                const std::string>& parameters)), (override));
+
+    MOCK_METHOD(const std::string&, getName, (), (const, override));
+};
+
+TEST(ReaderPluginTest, ReaderPlugin)
+{
+    RP_ReaderPluginMock plugin;
+}
